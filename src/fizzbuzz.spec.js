cbb098a9ad1f9603323598cd4f0d9468f4a9585b
nicolegs22 <nicole.gomez@ucb.edu.bo>
2025-12-11 16:43:17 -0400

    Ejercicio FIzBuzz se implemento mas test

--- src/fizzbuzz.spec.js
+++ src/fizzbuzz.spec.js
@@ -27,3 +27,44 @@ describe("FizzBuzz — Casos Negativos", () => {
     expect(() => fizzBuzz(-3)).toThrow("Número negativo no permitido");
   });
 });
+
+describe("FizzBuzz — Casos Adicionales", () => {
+
+  test("cero devuelve FizzBuzz (si tu lógica lo permite)", () => {
+    expect(fizzBuzz(0)).toBe("FizzBuzz");
+  });
+
+
+  test("número muy grande múltiplo de 5", () => {
+    expect(fizzBuzz(5000)).toBe("Buzz");
+  });
+
+  test("número muy grande múltiplo de 15", () => {
+    expect(fizzBuzz(15000)).toBe("FizzBuzz");
+  });
+
+  test("valor undefined lanza error", () => {
+    expect(() => fizzBuzz(undefined)).toThrow("Entrada no numérica");
+  });
+
+  test("valor null lanza error", () => {
+    expect(() => fizzBuzz(null)).toThrow("Entrada no numérica");
+  });
+
+  test("boolean true lanza error", () => {
+    expect(() => fizzBuzz(true)).toThrow("Entrada no numérica");
+  });
+
+  test("boolean false lanza error", () => {
+    expect(() => fizzBuzz(false)).toThrow("Entrada no numérica");
+  });
+
+  test("array lanza error", () => {
+    expect(() => fizzBuzz([3])).toThrow("Entrada no numérica");
+  });
+
+  test("objeto lanza error", () => {
+    expect(() => fizzBuzz({ num: 3 })).toThrow("Entrada no numérica");
+  });
+
+});
